d5f9362515fe720787b64e9094cb9b87ab14c0ae
“[Khalid <[khalid.haiddou01@gmail.com]>
2023-11-21 00:12:55 +0100

    new update in database

--- MyResources.sql
+++ MyResources.sql
@@ -5,3 +5,31 @@ CREATE TABLE Projet ( ProjetID INT PRIMARY KEY , NomProjet VARCHAR (20) , Descri
 CREATE TABLE Ressource ( RessourceId INT PRIMARY KEY )
 CREATE TABLE Catégorie ( CategoryID INT PRIMARY KEY , CategoryName VARCHAR (20) )
 CREATE TABLE Subcategory ( SubcategoryID INT PRIMARY KEY )
+ALTER TABLE Squad 
+ADD userID INT
+ALTER TABLE squad
+ADD FOREIGN KEY (userID) REFERENCES utilisateur(userID);
+ALTER TABLE squad 
+ADD squad_Name VARCHAR (30)
+ALTER TABLE squad 
+ADD ProjetID INT
+ALTER TABLE squad
+ADD FOREIGN KEY (ProjetID) REFERENCES projet(ProjetID);
+ALTER TABLE ressource 
+ADD SquadID INT 
+ALTER TABLE ressource
+ADD FOREIGN KEY (SquadID) REFERENCES squad(SquadID);
+ALTER TABLE ressource 
+ADD ProjetID INT
+ALTER TABLE ressource
+ADD FOREIGN KEY (ProjetID) REFERENCES projet(ProjetID);
+ALTER TABLE ressource
+ADD SubcategoryID INT 
+ALTER TABLE ressource
+ADD FOREIGN KEY (SubcategoryID) REFERENCES subcategory(SubcategoryID);
+ALTER TABLE catégorie
+ADD SubcategoryID INT 
+ALTER TABLE catégorie
+ADD FOREIGN KEY (SubcategoryID) REFERENCES subcategory(SubcategoryID);
+INSERT INTO utilisateur ( userID , nomUtilisateur , Mail)
+VALUES (1 , 'khalid' , 'khalid@gmail.com');
